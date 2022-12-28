2da6d2713938215a071dfdf62ba9321e49ebed01
Alessandro Tancredi <83644336+atancredi@users.noreply.github.com>
2022-12-28 12:34:03 +0100

    refactoring

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # python-data-utils
 Provides some useful properties for python classes
 
+## Modules
+ - Environment configuration
+ - Data Properties
+
 ### Requirements
  - <code>python-dotenv</code>
--- main.py
+++ main.py
@@ -1,6 +1,34 @@
-from datetime import datetime
+##############################################
+# ENV CONFIGURATION
+from dotenv import load_dotenv
+from os import environ as env
+from main import Serializable,Settable
+class Configuration(Serializable,Settable):
+    def __init__(self) -> None:
+        self.loaded = False
+        if not self.loaded:
+            load_dotenv()
+            self.loaded = True
+        self.variables = []
+        self.load()
 
-############################################## VERY IMPORTANT DATA PROPERTIES
+    # LOAD CONFIGURATION
+    def load(self):
+        self.getVariables()
+        for variable in self.variables:
+            self[variable] = env.get(variable)
+    
+    # GET VARIABLES NAMES
+    def getVariables(self):
+        self.variables = []
+        with open(".env") as f:
+            lines = f.readlines()
+        for line in lines:
+            self.variables.append(line.strip().split("=")[0])
+
+##############################################
+# DATA PROPERTIES
+from datetime import datetime
 class Serializable:
     def as_dict(self, obj = None):
         ret = {}
